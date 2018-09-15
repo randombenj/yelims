7b9393603ee0d7e865ba6e1cc13e79f0b8794b32
fliiiix <de-ch@hotmail.de>
2018-09-15 20:37:51 +0200

    cleanup

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,73 +1,88 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements'
+import {
+    Text,
+    View,
+    StyleSheet
+} from 'react-native';
+import {
+    FormLabel,
+    FormInput,
+    FormValidationMessage,
+    Button
+} from 'react-native-elements'
 const CONFIG = require('../config');
 
 export class LoginScreen extends React.Component {
     static navigationOptions = {
-      title: 'Login',
+        title: 'Login',
     };
     _login() {
         console.log("username: " + this.state.user + "pass: " + this.state.pass);
         fetch(CONFIG.API_URL + 'auth', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            username: this.state.user,
-            password: this.state.pass,
-        }),}).then((response) => response.json())
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: this.state.user,
+                    password: this.state.pass,
+                }),
+            }).then((response) => response.json())
             .then((responseJson) => {
-                if (responseJson.ok){
+                if (responseJson.ok) {
                     console.log("user login");
                     console.log(responseJson);
-                    this.props.navigation.navigate('Home', {authtoken: responseJson.data.token, authrefresh: responseJson.data.refresh});
-                }
-                else {
+                    this.props.navigation.navigate('Home', {
+                        authtoken: responseJson.data.token,
+                        authrefresh: responseJson.data.refresh
+                    });
+                } else {
                     console.error(responseJson);
                 }
-          })
-        .catch((error) => {
-          console.error(error);
-      });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     };
     _signup() {
         console.log("username: " + this.state.user + "pass: " + this.state.pass);
         fetch(CONFIG.API_URL + '/register', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            username: this.state.user,
-            password: this.state.pass,
-        }),
-        }).then((response) => response.json())
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: this.state.user,
+                    password: this.state.pass,
+                }),
+            }).then((response) => response.json())
             .then((responseJson) => {
-                if (responseJson.ok){
+                if (responseJson.ok) {
                     console.log("user create");
                     this._login();
-                }
-                else {
+                } else {
                     console.error(responseJson);
                 }
-          })
-        .catch((error) => {
-          console.error(error);
-      });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     };
     handle_user = (text) => {
-      this.setState({ user: text })
+        this.setState({
+            user: text
+        })
     };
     handle_pass = (text) => {
-      this.setState({ pass: text })
+        this.setState({
+            pass: text
+        })
     };
     render() {
         return (
-            <View style={[styles.container, styles.horizontal, {paddingTop: 15}]}>
+            <View style={[styles.container]}>
             <Text style={styles.title}>📓😀</Text>
 
             <FormLabel>Username</FormLabel>
@@ -76,21 +91,22 @@ export class LoginScreen extends React.Component {
             <FormLabel>Password</FormLabel>
             <FormInput onChangeText = {this.handle_pass} />
 
-            <Button onPress={() => this._login()} buttonStyle={styles.login} rightIcon={{name: 'arrow-forward'}} title='Login' />
-            <Button onPress={() => this._signup()} buttonStyle={styles.login} rightIcon={{name: 'arrow-forward'}} title='Sign up' />
+            <Button onPress={this._login()} buttonStyle={styles.login} rightIcon={{name: 'arrow-forward'}} title='Login' />
+            <Button onPress={this._signup()} buttonStyle={styles.login} rightIcon={{name: 'arrow-forward'}} title='Sign up' />
             </View>
-            );
-        }
+        );
     }
+}
 
 
-    const styles = StyleSheet.create({
-      container: {
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
         backgroundColor: '#fff',
         alignSelf: 'stretch',
         margin: 10,
-        marginRight: 10
+        marginRight: 10,
+        paddingTop: 15
     },
     contentContainer: {
         alignItems: 'center',
@@ -100,14 +116,14 @@ export class LoginScreen extends React.Component {
         flexDirection: "row",
     },
     login: {
-       marginTop: 15,
-   },
-   title: {
-    fontWeight: 'bold',
-    marginTop: 15,
-    marginBottom: 20,
-    color: "#333",
-    textAlign: 'center',
-    fontSize: 80,
-},
+        marginTop: 15,
+    },
+    title: {
+        fontWeight: 'bold',
+        marginTop: 15,
+        marginBottom: 20,
+        color: "#333",
+        textAlign: 'center',
+        fontSize: 80,
+    },
 });
