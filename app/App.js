c71009fca54cae75688b65b4fcf2dae732a0fbd1
eddex <eddex@outlook.com>
2018-09-15 11:53:32 +0200

    added empty MapScreen

--- app/App.js
+++ app/App.js
@@ -2,9 +2,10 @@ import {
   createStackNavigator,
 } from 'react-navigation';
 import { MainScreen } from './MainScreen';
+import { MapScreen } from './MapScreen';
 
 const App = createStackNavigator({
   Home: { screen: MainScreen },
+  Map: { screen: MapScreen },  
 });
-
 export default App;
