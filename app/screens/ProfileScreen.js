fde69ee6d96b182c99181323a40df7190154e78f
eddex <eddex@outlook.com>
2018-09-15 22:50:53 +0200

    fix profile screen top margin

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export class ProfileScreen extends React.Component {
-    static navigationOptions = {
-      title: 'Profile',
-    };
-    render() {
-        return (
-            <Text>This is the profile screen..</Text>
-        );
-    }
+  static navigationOptions = {
+    title: 'Profile',
+  };
+  render() {
+    return (
+      <View style={styles.view}>
+        <Text>This is the profile screen..</Text>
+      </View>
+    );
   }
+}
+
+const styles = StyleSheet.create({
+  view: {
+    marginTop: 24,
+  },
+});
