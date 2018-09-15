273b03dd6b7a612c673142421018e02a0d6f803c
eddex <eddex@outlook.com>
2018-09-15 13:06:09 +0200

    fix map screen description and added some stuff to search tab

--- app/screens/SearchScreen.js
+++ app/screens/SearchScreen.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StatusBar, SafeAreaView } from 'react-native';
 
 export class SearchScreen extends React.Component {
     static navigationOptions = {
       title: 'Search',
     };
+    componentDidMount() {
+        let isAndroid = true;
+        this._navListener = this.props.navigation.addListener('didFocus', () => {
+            StatusBar.setBarStyle('dark-content');
+            isAndroid && StatusBar.setBackgroundColor('#ff00ff');
+        });
+    }
+    
+    componentWillUnmount() {
+        this._navListener.remove();
+    }
     render() {
         return (
-            <Text>This is the search screen..</Text>
+            <SafeAreaView style={[{ backgroundColor: '#00ff00' }]}>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+                <Text>ASDTEST</Text>
+            </SafeAreaView>
         );
     }
-  }
+}
