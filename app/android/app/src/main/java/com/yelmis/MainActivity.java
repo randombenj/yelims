8f8f09504f0799a3f71ac9958676e2101611da02
Benj Fassbind <randombenj@gmail.com>
2018-09-18 21:41:03 +0200

    Rename yelmis to yelims ...

--- app/android/app/src/main/java/com/yelmis/MainActivity.java
+++ app/android/app/src/main/java/com/yelmis/MainActivity.java
@@ -1,4 +1,4 @@
-package com.yelmis;
+package com.yelims;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "yelmis";
+        return "yelims";
     }
 }
--- app/android/app/src/main/java/com/yelmis/MainApplication.java
+++ app/android/app/src/main/java/com/yelmis/MainApplication.java
@@ -1,4 +1,4 @@
-package com.yelmis;
+package com.yelims;
 
 import android.app.Application;
 
